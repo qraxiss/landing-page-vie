eb6a928a7028f02c234ecd0ca0b553fa01659f3f
qraxiss <qraxiss@gmail.com>
2024-05-02 21:41:13 +0300

    metadata changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,21 +5,20 @@ import "@/styles/modal-video.css";
 import "swiper/css/bundle";
 import "react-circular-progressbar/dist/styles.css";
 
-
 export const metadata = {
-  title: 'Vie',
-  description: 'Vie - Multi-Purpose React.js Next.js Template',
-  keywords: ['HTML5', 'Template', 'Vie', 'Multi-Purpose', 'themeforest'],
+  title: "Shopcek",
+  description: "Developed for the transition of the next billion to crypto.",
+  keywords: ["shopcek", "crypto", "blockchain"],
   icons: {
     icon: "/assets/imgs/favicon.ico",
-    shortcut: "/assets/imgs/favicon.ico"
+    shortcut: "/assets/imgs/favicon.ico",
   },
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
